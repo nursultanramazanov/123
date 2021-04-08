fd76606755ad97daea2103003455b43cce820ec7
nursultanramazanov <80708181+nursultanramazanov@users.noreply.github.com>
2021-04-08 12:24:06 +0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+// const isinfected = false;
+// if(isinfected){document.getElementById('secbut').addEventListener("click",()=>{
+//  document.getElementsByClassName('mainmid')[0].getElementsByTagName('img')[0].src= "images/nonsecure.png";
+//  document.getElementsByClassName('mainmid')[0].getElementsByTagName('p')[0].innerHTML = "YOURE IN DEEP SHIT";
+// });}else{
+//   document.getElementById('secbut').addEventListener("click",()=>{
+//     document.getElementsByClassName('mainmid')[0].getElementsByTagName('img')[0].src= "images/secure.png";
+//     document.getElementsByClassName('mainmid')[0].getElementsByTagName('p')[0].innerHTML = "NO VIRUSES FOUND"
+//   });
+// }
+// if(isinfected){document.getElementById('secbut').addEventListener("click",()=>{
+ // document.getElementsByClassName('mainmid')[0].getElementsByTagName('img')[0].src= "images/nonsecure.png";
+ // document.getElementsByClassName('mainmid')[0].getElementsByTagName('p')[0].innerHTML = fmalw[0]+fmalw[01];
+// });}else{
+//   document.getElementById('secbut').addEventListener("click",()=>{
+    // document.getElementsByClassName('mainmid')[0].getElementsByTagName('img')[0].src= "images/secure.png";
+    // document.getElementsByClassName('mainmid')[0].getElementsByTagName('p')[0].innerHTML = "NO VIRUSES FOUND"
+//   });
+// }
